7456c5f5f5f290a4295b3ca2ed12862dc69fca44
powerbuoy <powerbuoy@gmail.com>
2021-03-19 17:15:03 +0100

    MOar

--- three/app.js
+++ three/app.js
@@ -144,17 +144,17 @@ class Bg3d {
 	////////////////////
 	// Scroll camera pos
 	scrollCameraPos () {
-		document.querySelectorAll('[data-camera-pos]').forEach(el => {
-			new IntersectionObserver(entries => entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					const newPos = JSON.parse(entry.target.dataset.cameraPos);
+		const observer = new IntersectionObserver(entries => entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const newPos = JSON.parse(entry.target.dataset.cameraPos);
 
-					new TWEEN.Tween(this.camera.position).to({x: newPos.x, y: newPos.y, z: newPos.z}, this.config.cameraTransitionDuration).easing(this.config.easing).start();
-					new TWEEN.Tween(this.camera.rotation).to({x: newPos.rx, y: newPos.ry, z: newPos.rz}, this.config.cameraTransitionDuration).easing(this.config.easing).start();
-					new TWEEN.Tween(this.scene.position).to({x: 0, y: 0}, this.config.cameraTransitionDuration).easing(this.config.easing).start();
-				}
-			}), {threshold: 0, rootMargin: '0% 0% -15%'}).observe(el);
-		});
+				new TWEEN.Tween(this.camera.position).to({x: newPos.x, y: newPos.y, z: newPos.z}, this.config.cameraTransitionDuration).easing(this.config.easing).start();
+				new TWEEN.Tween(this.camera.rotation).to({x: newPos.rx, y: newPos.ry, z: newPos.rz}, this.config.cameraTransitionDuration).easing(this.config.easing).start();
+				new TWEEN.Tween(this.scene.position).to({x: 0, y: 0}, this.config.cameraTransitionDuration).easing(this.config.easing).start();
+			}
+		}), {threshold: 0.25});
+
+		document.querySelectorAll('[data-camera-pos]').forEach(el => observer.observe(el));
 	}
 
 	/////////
@@ -206,17 +206,19 @@ Splitting({
 
 ////////////
 // Scrollspy
+const scrollspyObserver = new IntersectionObserver(entries => entries.forEach(entry => {
+	if (entry.isIntersecting) {
+		entry.target.classList.add('in-view');
+	}
+	else {
+		entry.target.classList.remove('in-view');
+	}
+}), {threshold: 0.25});
+
 document.querySelectorAll('section').forEach(el => {
 	Array.from(el.children).forEach((child, index) => child.style.setProperty('--scrollspy-el-index', index));
 
-	new IntersectionObserver(entries => entries.forEach(entry => {
-		if (entry.isIntersecting) {
-			entry.target.classList.add('in-view');
-		}
-		else {
-			entry.target.classList.remove('in-view');
-		}
-	}), {threshold: 0, rootMargin: '0% 0% -15%'}).observe(el);
+	scrollspyObserver.observe(el);
 });
 
 ////////////
@@ -236,4 +238,21 @@ allThemeButtons.forEach(el => {
 		document.documentElement.classList.add('theme-' + theme);
 		el.classList.add('active');
 	});
+});
+
+////////////////////
+// Highlight visible
+document.querySelectorAll('[data-highlight-visible]').forEach(el => {
+	const links = el.querySelectorAll('a[href^="#"]');
+	const targets = document.querySelectorAll(Array.from(links).map(link => link.getAttribute('href')).join(', '));
+	const observer = new IntersectionObserver(entries => entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			console.log(entry.target.id);
+		}
+		else {
+			console.log('OUT: ' + entry.target.id);
+		}
+	}), {threshold: 0.25});
+
+	targets.forEach(t => observer.observe(t));
 });
